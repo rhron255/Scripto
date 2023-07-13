9b76b64344e7e4591ebdb4569a7ddfcc74888696
ronhasleton <ronhasleton@gmail.com>
2023-07-13 18:25:57 +0300

    - Made the auto_cli decorator work as a registration function that can take configuration values! - AutoCli class now has an instance. - No longer generating a parent parser, args are generated per argument and per parser.

--- AutoCLI.py
+++ AutoCLI.py
@@ -4,39 +4,60 @@ from argparse import ArgumentParser
 from types import FunctionType
 from typing import List, Tuple
 
-from FuncUtils import generate_parser, make_kebab_case
+from FuncUtils import generate_parser, make_kebab_case, get_description, generate_parser_definitions, \
+    generate_action_settings
 
 
-def auto_cli(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        arg_parser = generate_parser(func)
-        return func(**vars(arg_parser.parse_args()))
+# def auto_cli(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         arg_parser = generate_parser(func)
+#         return func(**vars(arg_parser.parse_args()))
+#
+#     return wrapper
 
-    return wrapper
+def auto_cli(*config_args, **config_kwargs):
+    def registration_function(func: FunctionType):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        AutoCli.register_func(func)
+        return wrapper
+
+    return registration_function
 
 
 class AutoCli:
     _description: str
-    _sub_parsers: List[Tuple[FunctionType, ArgumentParser]] = []
+    _functions: List[FunctionType] = []
+    _instance = None
 
     def __init__(self, description):
         self._description = description
+        if AutoCli._instance is None:
+            AutoCli._instance = self
 
     def register_function(self, func: FunctionType):
-        self._sub_parsers.append((func, generate_parser(func)))
+        self._functions.append(func)
 
     def run(self):
-        if len(self._sub_parsers) == 0:
+        if len(self._functions) == 0:
             raise ValueError('No functions registered...')
-        elif len(self._sub_parsers) == 1:
-            function, parser = self._sub_parsers[0]
-            function(**vars(parser.parse_args()))
+        elif len(self._functions) == 1:
+            function = self._functions[0]
+            function(**vars(generate_parser(function).parse_args()))
         else:
             parent_parser = argparse.ArgumentParser(description=self._description)
             sub = parent_parser.add_subparsers(required=True)
-            for func, inner_parser in self._sub_parsers:
-                inner_parser.set_defaults(func=func)
-                sub.add_parser(name=make_kebab_case(func.__name__), parents=[inner_parser], add_help=False)
+            for func in self._functions:
+                name, settings = generate_parser_definitions(func)
+                sub_parser = sub.add_parser(name, **settings)
+                for name, settings in generate_action_settings(func):
+                    sub_parser.add_argument(name, **settings)
             args = parent_parser.parse_args()
             args.func(args)
+
+    @classmethod
+    def register_func(cls, func: FunctionType):
+        AutoCli._instance.register_function(func)
--- Test.py
+++ Test.py
@@ -1,8 +1,10 @@
 # A test document with function stubs to test
-from AutoCLI import auto_cli
+from AutoCLI import auto_cli, AutoCli
+
+app = AutoCli("Test")
 
 
-@auto_cli
+@auto_cli()
 def test_func(param_a: int, param_b: int):
     """
     Some fancy doctstring describing the function.
@@ -13,7 +15,7 @@ def test_func(param_a: int, param_b: int):
     pass
 
 
-@auto_cli
+# @auto_cli
 def fail_func(param_a: int, param_b: int):
     """
     Some fancy doctstring describing the function.
@@ -23,6 +25,15 @@ def fail_func(param_a: int, param_b: int):
     pass
 
 
+@auto_cli()
+def bool_func(b: bool):
+    """
+    Funky
+    :param b: does stuff
+    :return: Nope
+    """
+    print("success" if b else "failure")
+
+
 if __name__ == '__main__':
-    test_func(1, 2)
-    fail_func(1, 2)
+    app.run()
