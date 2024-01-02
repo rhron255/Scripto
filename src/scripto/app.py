989b7a85be79108c04f8a7f7159f1941ac4750b4
ronha <ronhasleton@gmail.com>
2024-01-02 23:02:42 +0200

    Create Proper CI-CD: - Linted entire project:     - Added lots of missing documentation.     - Switched to cleaner isinstance function instead of type checking.     - Changed to proper try except instead of the weird previous shenanigans.     - Changed dict declarations.     - Fixed possible error with description parser.     - Changed pylint fail threshold.

--- src/scripto/app.py
+++ src/scripto/app.py
@@ -1,14 +1,23 @@
+"""
+The main module of the scripto package.
+Contains the Scripto class, which is the main holder of the script data/metadata.
+"""
 import argparse
 import functools
 import logging
+from argparse import ArgumentParser
 from types import FunctionType
 from typing import List
 
-from scripto.ArgParserUtils import add_logging_flags, generate_parser_definitions, generate_action_settings
+from scripto.ArgParserUtils import add_logging_flags, generate_parser_definitions, \
+    generate_action_settings
 from scripto.FuncUtils import validate_parameters_in_docstring, get_argument_names, make_kebab_case
 
 
 class Scripto:
+    """
+    Holder and runner class for scripts.
+    """
     _description: str
     _silence: bool
     _functions: List[FunctionType] = []
@@ -20,7 +29,11 @@ class Scripto:
         self._silence = suppress_warnings
         self._use_logger = auto_log
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Parses the functions into an ArgumentParser and runs the script accordingly.
+        :return: None
+        """
         parser = argparse.ArgumentParser(description=self._description, conflict_handler='resolve',
                                          formatter_class=argparse.RawDescriptionHelpFormatter)
         if len(self._functions) == 0:
@@ -46,17 +59,23 @@ class Scripto:
             func_args.pop('log_level')
         args.func(**func_args)
 
-    def add_function_to_parser(self, func, parser):
+    def add_function_to_parser(self, func: FunctionType, parser: ArgumentParser):
+        """
+        Registers a new function into the parser definitions.
+        :param func: The function to register.
+        :param parser: The parser that the function should be added to.
+        :return: None
+        """
         for name, settings in generate_action_settings(func):
             target_name = name
-            if type(name) is list:
+            if isinstance(name, list):
                 target_name = name[0][2:].replace('-', '_')
             if target_name in self._arg_initializers[func.__name__]:
                 argument_values = self._arg_initializers[func.__name__][target_name]
-                if type(argument_values) is dict:
+                if isinstance(argument_values, dict):
                     parser.description += f'\n\t{target_name} - {settings["help"]}'
                     should_be_required = True
-                    if 'default' in settings and settings['default'] not in argument_values.values():
+                    if settings.get('default') not in argument_values.values():
                         should_be_required = False
                         parser.set_defaults(**{f'{target_name}': settings['default']})
                         argument_values[target_name] = settings['default']
@@ -65,15 +84,15 @@ class Scripto:
                         mutex_group.add_argument(f'--{make_kebab_case(value[0])}', f'-{make_kebab_case(value[0])[0]}',
                                                  dest=target_name, action='store_const', const=value[1],
                                                  help=f'Sets {target_name} to {value[1]}')
-                elif type(argument_values) is list:
-                    if 'default' in settings and settings['default'] not in argument_values:
+                elif isinstance(argument_values, list):
+                    if settings.get('default') not in argument_values:
                         argument_values.append(settings['default'])
-                    if type(name) is list:
+                    if isinstance(name, list):
                         parser.add_argument(*name, **settings, choices=sorted(argument_values))
                     else:
                         parser.add_argument(name, **settings, choices=sorted(argument_values))
             else:
-                if type(name) is list:
+                if isinstance(name, list):
                     parser.add_argument(*name, **settings)
                 else:
                     parser.add_argument(name, **settings)
@@ -81,15 +100,27 @@ class Scripto:
             add_logging_flags(parser)
         parser.set_defaults(func=func)
 
-    def register(self, *config_args, **config_kwargs):
+    def register(self, /, **config_kwargs):
+        """
+        A function for registering new function in your script.
+        A parameter with a name as any argument your function takes will be consumed in the
+         following manner:
+         - If a list, will use the list to enforce a set of values.
+         - If a dict, will use the dict to build a set of flags to set those values.
+         In both cases, the defaults set in the signature are also included in the enforced values.
+        :param config_kwargs:
+        :return:
+        """
+
         def registration_function(func: FunctionType):
             @functools.wraps(func)
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
 
-            parameter_validation_exception = validate_parameters_in_docstring(func, self._silence)
-            if parameter_validation_exception is not None:
-                raise parameter_validation_exception
+            try:
+                validate_parameters_in_docstring(func, self._silence)
+            except TypeError as e:
+                raise e
             self._functions.append(func)
             self._arg_initializers[func.__name__] = dict(config_arg for config_arg in
                                                          config_kwargs.items() if
