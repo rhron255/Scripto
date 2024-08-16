b89c103cb14983ab5b0dc9c7d646e9300fc919f7
rhron255 <ronhasleton@gmail.com>
2024-08-16 13:07:06 +0300

    Fixed default arg handling for lists: The name was generated as a list because of the list handling making it a flag. Now using original name and regenerating the flag values.
    Updated examples with default list values.

--- exmaples/Examples.py
+++ exmaples/Examples.py
@@ -31,8 +31,9 @@ def bool_func(super_long_parameter_name: bool):
     """
     print("success" if super_long_parameter_name else "failure")
 
+
 @script.register()
-def list_func(opt_arg: list[str]):
+def list_func(opt_arg: list[str] = ("one", "two", "three")):
     """
     Takes all command line arguments and prints them!
     :param opt_arg: A list of strings to print
